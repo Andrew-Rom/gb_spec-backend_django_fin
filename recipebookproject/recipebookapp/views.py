510b5da58d315f5aaac8c48fd83365da80564135
Andrew-Rom <1811mail@gmail.com>
2024-03-06 20:39:23 +0300

    fixed empty range for randrange error

--- recipebookproject/recipebookapp/views.py
+++ recipebookproject/recipebookapp/views.py
@@ -38,7 +38,7 @@ def index(request):
     if count == 1:
         recipes = [Recipe.objects.all()[0]]
         return render(request, "recipebookapp/index.html", {'recipes': recipes})
-    if 2 < count < number_of_cards:
+    if 1 < count < number_of_cards:
         number_of_cards = count
     while len(recipes) < number_of_cards:
         recipe = get_random_recipe()
